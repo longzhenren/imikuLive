7c1a7376ee867af67a39c019d90ea368e099fe69
Operacon <anlan18437@hotmail.com>
2022-04-08 16:19:53 +0800

    初始化 springboot

---
+++ src/main/java/fun/imiku/live/ImikuLiveApplication.java
@@ -0,0 +1,13 @@
+package fun.imiku.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImikuLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImikuLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/imiku/live/ImikuLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.imiku.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImikuLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
